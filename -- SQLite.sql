cb60fa6e64e438ff9d8f38b45a623e2e0c2a1e55
EvgrafovaLiza <74619556+EvgrafovaLiza@users.noreply.github.com>
2023-12-21 18:30:56 +0100

    Add files via upload

---
+++ -- SQLite.sql
@@ -0,0 +1,11 @@
+-- SQLite
+CREATE TABLE IF NOT EXISTS FormData (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+arrivalDate TEXT, 
+departureDate TEXT, 
+petsNumber INTEGER, 
+roomType TEXT,
+userName TEXT,
+userTelephone TEXT,
+userEmail TEXT
+);
